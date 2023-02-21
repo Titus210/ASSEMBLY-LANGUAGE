a92d53b0b5e95a5d8eea6fc37daae0ee6dc632e5
Titus Kiplagat <Tituskiplagat210@gmail.com>
2023-02-21 18:37:23 +0300

    Coded Low level

---
+++ ASSEMBLY LANGUAGE/Introduction/sum/README.md
@@ -0,0 +1,3 @@
+## Sum
+This is a simple code to sum two integers in both c and assembly language.<br/>
+Its a descriptive way to show the sum of two integers using assembly language
---
+++ ASSEMBLY LANGUAGE/Introduction/sum/add.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+// main menu
+void main(){
+	// variables declaration
+	int a, b, sum;
+
+	//get input of a and b
+	printf("Enter value of a: ");
+	scanf("%d",&a);
+	printf("Enter value of b: ");
+	scanf("%d", &b);
+
+	// sum of a and b
+
+	sum = a + b;
+
+	printf("The sum of The two numberd is %d", sum);
+}
